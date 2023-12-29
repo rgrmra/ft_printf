ffc698c0a368bddc4d7e05e51b26b1e79829649c
Roger de Moura <rde-mour@students.42sp.org.br>
2023-12-28 21:58:26 -0300

    Fixed ft_build_pointer.c with a crazy verification

--- srcs/ft_build_pointer.c
+++ srcs/ft_build_pointer.c
@@ -6,7 +6,7 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 14:54:21 by rde-mour          #+#    #+#             */
-/*   Updated: 2023/10/29 17:33:08 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2023/12/28 21:48:49 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 static void	ft_putnbr(unsigned long nbr, const char *base, t_node **list)
 {
-	if (nbr < 0)
-		nbr = 4294967295 + nbr + 1;
 	if (nbr < 16)
 		lstadd_back(list, lstnew(*(base + nbr)));
 	if (nbr >= 16)
